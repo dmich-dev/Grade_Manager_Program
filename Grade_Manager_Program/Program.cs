4970edcee77c37777c9070a7384f7089c10e7469
David Michaud <michaud671468@newmanu.edu>
2025-12-01 10:24:36 -0600

    initial commit

---
+++ Grade_Manager_Program/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace RefactoringHomework
+{
+    public class GradeManager : IGradeManager
+    {
+        private double passingGrade = 50.0;
+
+        public double PassingGrade { get => passingGrade; set => passingGrade = value; }
+
+        public void LogGradeProcessingMessage()
+        {
+            Console.WriteLine("Working on grades...");
+        }
+
+        public void processGrades(string studentName, double[] grades)
+        {
+            Console.WriteLine("Processing grades for " + studentName);
+
+            double total = CalculateTotal(grades);
+            double average = CalculateAverage(grades, total);
+            Console.WriteLine("Average: " + average);
+
+            if (average >= PassingGrade)
+            {
+                Console.WriteLine("Status: Pass");
+            }
+            else
+            {
+                Console.WriteLine("Status: Fail");
+            }
+        }
+
+        private static double CalculateAverage(double[] grades, double total)
+        {
+            return total / grades.Length;
+        }
+
+        private static double CalculateTotal(double[] grades)
+        {
+            double total = 0;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                total += grades[i];
+            }
+
+            return total;
+        }
+
+        public double calculateFinalGrade(double average, double bonus)
+        {
+            return average + bonus;
+        }
+    }
+}
